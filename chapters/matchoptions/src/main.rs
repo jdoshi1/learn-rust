8cd6cbedb1e93f2424bf1faa7ab78af71f33b96f
Jinesh Doshi <jdoshi1@asu.edu>
2020-12-30 04:07:41 -0800

    match, Option and if let chapters

--- chapters/matchoptions/src/main.rs
+++ chapters/matchoptions/src/main.rs
@@ -24,10 +24,31 @@ fn value_in_cents(coin: &Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+// Non-exhaustive function. Needs None arm.
+// fn plus_one_more(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         Some(i) => Some(i + 1),
+//     }
+// }
+
 fn main() {
     println!("Match Coins!");
     let z = [Coin::Quarter(UsState::Alabama), Coin::Penny, Coin::Nickel, Coin::Dime, Coin::Quarter(UsState::Alaska)];
     for (i, c) in z.iter().enumerate() {
-        println!("Coin: {} -> Denomination: {}", i, value_in_cents(c));
+        let x = value_in_cents(c);
+        println!("Coin: {} -> Denomination: {}", i, x);
     }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("five:{:?} six:{:?} None:{:?}", five, six, none);
 }
